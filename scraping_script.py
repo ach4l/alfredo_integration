d62b65fe9b8363715791c10fae455861cc1ddd8a
Achal Agarwal <1169@MECHYD.AD>
2019-12-03 03:50:44 +0530

    Reading from To Do list and saving it in a folder of the userid

--- scraping_script.py
+++ scraping_script.py
@@ -1,27 +1,47 @@
 import urllib.request
 from bs4 import BeautifulSoup
-from pytube import YouTube 
+from pytube import YouTube
+import os
 
 # Making the query
-query = 'Butta Gandu'
-query = query.replace(" ", "+")
-url = "http://m.youtube.com/results?search_query=" + query
-response = urllib.request.urlopen(url)
-html = response.read()
 
-# Getting links and Saving the first
-soup = BeautifulSoup(html, 'html.parser')
-downloaded = 0
-for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
-    url = 'https://m.youtube.com' + vid['href']
-    if downloaded == 0:
-        downloaded = 1
-        
-        yt = YouTube(url)
-        #yt.set_filename(vid.contents)
-        stream = yt.streams.first()
-        stream.download()
-        
-        print('https://m.youtube.com' + vid['href'])
-        print('Saved as')
-print('Done')
+def youtube_scraper(query,userid):
+    my_dir = os.path.expanduser('~/'+userid)
+
+    #SAVE_PATH = '/' +str(userid)+ '/'
+    
+    query = query.replace(" ", "+")
+    url = "http://m.youtube.com/results?search_query=" + query
+    response = urllib.request.urlopen(url)
+    html = response.read()
+
+    # Getting links and Saving the first
+    soup = BeautifulSoup(html, 'html.parser')
+    downloaded = 0
+    for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
+        url = 'https://m.youtube.com' + vid['href']
+        if downloaded == 0:
+            downloaded = 1  # Downloading only one for now
+            
+            yt = YouTube(url)
+            #yt.set_filename(vid.contents)
+            stream = yt.streams.first()
+            stream.download('1/')
+            
+            print('https://m.youtube.com' + vid['href'])
+            print('Saved as')
+    print('Done')
+
+    return
+
+# Reading the todo csv line by line
+
+import csv
+
+with open('todo.csv', 'r') as f:
+    reader = csv.reader(f)
+    todo_list = list(reader)
+    for item in todo_list:
+        if item[2] == 'youtube':
+            youtube_scraper(item[3],item[0])
+
