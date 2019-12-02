a1218f8ff0d8ebad58d7c8f597760b85645d68ed
Achal Agarwal <1169@MECHYD.AD>
2019-12-03 00:46:01 +0530

    syncing pi and local

---
+++ README.txt
@@ -0,0 +1,15 @@
+To Connect Raspberry Pi
+
+- Name your wifi hotspot as rpi_hotspot_23
+- The network password is maximus13
+
+The device should automatically connect (See in the hotspot settings)
+
+- ssh to the ip address shown in connected devices (Note : This changes everytime)
+- Username - pi, password - raspberry
+
+Voila, you should be able to develop!
+
+To activate virtual environment for backend
+
+$ source cacheon_backend/bin/activate
---
+++ scraping_script.py
@@ -0,0 +1,26 @@
+import urllib.request
+from bs4 import BeautifulSoup
+from pytube import YouTube 
+
+# Making the query
+query = 'hutta'
+url = "http://m.youtube.com/results?search_query=" + query
+response = urllib.request.urlopen(url)
+html = response.read()
+
+# Getting links and Saving the first
+soup = BeautifulSoup(html, 'html.parser')
+downloaded = 0
+for vid in soup.findAll(attrs={'class':'yt-uix-tile-link'}):
+    url = 'https://m.youtube.com' + vid['href']
+    if downloaded == 0:
+        downloaded = 1
+        
+        yt = YouTube(url)
+        #yt.set_filename(vid.contents)
+        stream = yt.streams.first()
+        stream.download()
+        
+        print('https://m.youtube.com' + vid['href'])
+        print('Saved as')
+print('Done')
